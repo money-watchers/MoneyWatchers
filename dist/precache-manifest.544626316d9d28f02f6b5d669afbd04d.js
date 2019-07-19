c5f33be7aa7f7e5bed321bc204a21c2fb1e76025
Carlos Antonio <carlosantoniooln@gmail.com>
2019-07-18 21:49:39 -0300

    start

---
+++ dist/precache-manifest.544626316d9d28f02f6b5d669afbd04d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/vue-argon-dashboard/img/brand/white.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-argon-dashboard/robots.txt"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/vue-argon-dashboard/img/theme/angular.jpg"
+  },
+  {
+    "revision": "abc4711559fb8b9fa502",
+    "url": "/vue-argon-dashboard/js/chunk-vendors.17e61df6.js"
+  },
+  {
+    "revision": "562e9190d434c96ebdc8",
+    "url": "/vue-argon-dashboard/js/demo.01fbb86f.js"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/vue-argon-dashboard/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/vue-argon-dashboard/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/vue-argon-dashboard/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/vue-argon-dashboard/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "c6875567483b319711070f8733fd2ce7",
+    "url": "/vue-argon-dashboard/index.html"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/vue-argon-dashboard/img/brand/green.png"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/vue-argon-dashboard/img/brand/favicon.png"
+  },
+  {
+    "revision": "1d687150e7c14eae62a3",
+    "url": "/vue-argon-dashboard/js/app.c9f514ab.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/vue-argon-dashboard/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/vue-argon-dashboard/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/vue-argon-dashboard/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/vue-argon-dashboard/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/vue-argon-dashboard/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/vue-argon-dashboard/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/vue-argon-dashboard/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/vue-argon-dashboard/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/vue-argon-dashboard/img/theme/vue.jpg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/vue-argon-dashboard/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "1d687150e7c14eae62a3",
+    "url": "/vue-argon-dashboard/css/app.b09f4ae7.css"
+  }
+];
